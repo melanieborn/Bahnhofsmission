fd95f42e216554ec7cb793827f53d551e5eefa51
melanieborn <160163854+melanieborn@users.noreply.github.com>
2025-04-09 14:57:11 +0200

    code cleaning

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,25 +1,14 @@
 <template>
   <v-container>
 <v-row>
-  <v-col cols="2">
-    <div class="img">
-      <img src="@/assets/ls_logo.gif">
-    </div>
-  </v-col>
-
-    <v-col cols="9">
+    <v-col cols="12">
     <v-card class="banner">
-  Herzlich willkommen bei der Bahnhofsmission im Hauptbahnhof Berlin
+ObdachlosenWiki
   <WeatherApi />
 </v-card>
   </v-col>
 </v-row>
 <v-row>
-  <v-col cols="3">
-    <v-card class="cardstyle">
-      <MitarbeiterVerwaltung />
-    </v-card>
-  </v-col>
   <v-col cols="4">
     <v-card >
       <v-card-text class="cardstyle">
@@ -35,60 +24,16 @@
     </v-card>
   </v-col>
 </v-row>
-<!-- <v-row>
-  <v-col cols="4">
-    <v-card>
-      <v-card-text class="cardstyle">
-      <UnmsteigHilfe />
-    </v-card-text>
-    </v-card>
-  </v-col>
-  <v-col cols="4">
-    <v-card>
-      <v-card-text class="cardstyle">
-      <WeatherAPI />
-    </v-card-text>
-    </v-card>
-  </v-col>
-</v-row> -->
 <v-row>
   <v-col>
     <FooterComponent />
   </v-col>
 </v-row>
-<!--  <h1>{{ msg }}</h1> -->   
-    <!-- <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul> -->
   </v-container>
 </template>
 
 <script>
 import WeatherApi from '@/components/WeatherApi.vue'
-import MitarbeiterVerwaltung from '@/components/MitarbeiterVerwaltung.vue'
 import FooterComponent from '@/components/FooterComponent.vue'
 import ObdachlosenWiki from '@/components/ObdachlosenWiki.vue'
 import FirstSteps from '@/components/FirstSteps.vue'
@@ -96,7 +41,6 @@ export default {
   name: 'HelloWorld',
   components: {
     WeatherApi,
-    MitarbeiterVerwaltung,
     FooterComponent,
     ObdachlosenWiki,
     FirstSteps
