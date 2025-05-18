c427784c4a7e8ab60bcbfab0cab188f5b47198c4
melanieborn <beratungspraxis-bornkessel@web.de>
2025-05-18 09:30:37 +0200

    add template for components

---
+++ src/templateForComponents.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-container>
+        <v-card>
+            <v-card-title>
+                <v-card-text>
+                    
+                </v-card-text>
+            </v-card-title>
+        </v-card>
+
+    </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'NameComponent',
+  props: {
+    msg: String
+  }
+}
+</script>
