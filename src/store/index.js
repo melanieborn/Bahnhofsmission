8a670bcbc1938172faa2d45f22635fdea2d3ec20
melanieborn <beratungspraxis-bornkessel@web.de>
2025-05-18 09:21:53 +0200

    git ignore + code clean up

--- src/store/index.js
+++ src/store/index.js
@@ -2,15 +2,6 @@
 
 import Vue from 'vue'
 import Vuex from 'vuex'
-import entry from './modules/entry'
-import permissions from './modules/permissions'
-import profile from './modules/profile'
-import settings from './modules/settings'
-import credit from './modules/credit'
-import account from './modules/account'
-import errors from './modules/errors'
-import voucher from './modules/voucher'
-import organization from './modules/organization'
 import createPersistedState from 'vuex-persistedstate'
 import SecureLS from 'secure-ls'
 var ls = new SecureLS({
@@ -31,14 +22,6 @@ export default new Vuex.Store({
     })
   ],
   modules: {
-    account,
-    entry,
-    permissions,
-    profile,
-    settings,
-    credit,
-    errors,
-    voucher,
-    organization
+
   }
 })
