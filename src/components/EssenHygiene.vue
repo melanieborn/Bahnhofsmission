5c7ae7bc3d02725d54270c1a397a5161a905db15
melanieborn <beratungspraxis-bornkessel@web.de>
2025-07-27 19:25:10 +0200

    testing nav bar, entscheidung verschoben, beginn tabelle Berliner Tafel

--- src/components/EssenHygiene.vue
+++ src/components/EssenHygiene.vue
@@ -2,15 +2,22 @@
     <v-container>
 Komponente Essen / Hygiene / Schlafen
 hier werden Ausgabestellen in Tabellenform zur Verfügung gestellt
+Essen Berliner Tafel geordnet nach Bezirken
+<BerlinerTafel />
     </v-container>
 </template>
 
 <script>
+import BerlinerTafel from '@/components/Essen/BerlinerTafel.vue'
+
 
 export default {
   name: 'EssenHygiene',
   props: {
     msg: String
-  }
+  },
+  components: {
+    BerlinerTafel
+  },
 }
 </script>
